cacdb4f3f74a68ed003e00e1836c17232d2979bc
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2022-01-03 05:05:07 +0530

    Configure dependency injection

--- src/main/java/com/akshitbaunthy/springdemo/BaseballCoach.java
+++ src/main/java/com/akshitbaunthy/springdemo/BaseballCoach.java
@@ -2,9 +2,24 @@ package com.akshitbaunthy.springdemo;
 
 public class BaseballCoach implements Coach {
 
+    //    Define a private field for the dependency
+    private FortuneService fortuneService;
+
+    //    Define a constructor for dependency injection
+    public BaseballCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes on batting practice.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+
+//        Use FortuneService to get a fortune
+        return fortuneService.getFortune();
     }
 
 }
